673f7a908527eb14940db86b0aefc003cf249c04
xuancuongdoo <cuongxuando209@gmail.com>
2023-09-19 20:44:49 +0700

    WIP

--- config.py
+++ config.py
@@ -5,4 +5,15 @@ load_dotenv()
 config = {
     "google_api_key": os.environ.get("API_KEY"),
     "playlist_key": os.environ.get("PLAYLIST"),
+    "kafka": {
+        "bootstrap.servers": os.environ.get("BOOTSTRAP_SERVERS"),
+        "security.protocol": os.environ.get("SECURITY_PROTOCOL"),
+        "sasl.mechanism": os.environ.get("SASL_MECHANISM"),
+        "sasl.username": os.environ.get("SASL_USERNAME"),
+        "sasl.password": os.environ.get("SASL_PASSWORD")
+    },
+    "schema_registry": {
+        "url": os.environ.get("URL"),
+        "basic.auth.user.info": os.environ.get("BASIC_AUTH_USER_INFO")
+    }
 }
--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import requests
 import json
 from config import config
 from pprint import pformat
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka import SerializingProducer
+
+
+def summarize_video(video):
+    return {
+        "video_id": video["id"],
+        "title": video["snippet"]["title"],
+        # "description": video["snippet"]["localized"]["description"],
+        "view_count": int(video["statistics"].get("viewCount", 0)),
+        "likes": int(video["statistics"].get("likeCount", 0)),
+        "comments": int(video["statistics"].get("commentCount", 0)),
+    }
 
 
 def fetch_playlist_items_page(google_api_key, playlist_id, page_token=None):
@@ -19,7 +34,7 @@ def fetch_playlist_items_page(google_api_key, playlist_id, page_token=None):
         })
     payload = json.loads(response.text)
 
-    logging.debug("GOT %s", payload)
+    # logging.debug("GOT %s", payload)
     return payload
 
 
@@ -35,7 +50,7 @@ def fetch_videos_pages(google_api_key, video_id, page_token=None):
         })
     payload = json.loads(response.text)
 
-    logging.debug("GOT %s", payload)
+    # logging.debug("GOT %s", payload)
     return payload
 
 
@@ -62,15 +77,46 @@ def fetch_videos(google_api_key, playlist_id, page_token=None):
         yield from fetch_videos(google_api_key, playlist_id, next_page_token)
 
 
+def on_delivery(err, record):
+    pass
+
+
 def main():
     print("Script started.")
     logging.info("START")
+
+    schema_registry_client = SchemaRegistryClient(config["schema_registry"])
+    youtube_videos_value_schema = schema_registry_client.get_latest_version(
+        "youtube_videos-value")
+
+    kafka_config = config["kafka"] | {
+        "key.serializer": StringSerializer(),
+        "value.serializer": AvroSerializer(
+            schema_registry_client,
+            youtube_videos_value_schema.schema.schema_str,
+        ),
+    }
+
+    producer = SerializingProducer(kafka_config)
     google_api_key = config['google_api_key']
     playlist_id = config['playlist_key']
     for video_item in fetch_playlist_items(google_api_key, playlist_id):
         video_id = video_item["contentDetails"]["videoId"]
         for video in fetch_videos(google_api_key, video_id):
-            logging.info("GOT %s", pformat(video))
+            logging.info("GOT %s", pformat(summarize_video(video)))
+            producer.produce(
+                topic="youtube_videos",
+                key=video_id,
+                value={
+                    "TITLE": video["snippet"]["title"],
+                    "VIEWS": int(video["statistics"].get("viewCount", 0)),
+                    "LIKES": int(video["statistics"].get("likeCount", 0)),
+                    "COMMENTS": int(video["statistics"].get("commentCount", 0)),
+                },
+                on_delivery=on_delivery,
+            )
+
+    producer.flush()
 
 
 if __name__ == "__main__":
